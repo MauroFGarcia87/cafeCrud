78085929467c9f78f486a5bc2a2c9c98ad072356
unknown <maurofgarcia87@gmail.com>
2023-08-18 16:41:11 -0300

    agregamos el codigo para eliminar productos

--- src/components/productos/ItemProducto.js
+++ src/components/productos/ItemProducto.js
@@ -4,6 +4,47 @@ import Swal from "sweetalert2";
 import { Link } from "react-router-dom";
 
 const ItemProducto = (props) => {
+  const eliminarProducto = () => {
+    console.log(props.producto.id);
+    Swal.fire({
+      title: '¿Estas seguro que queires eliminar el producto?',
+      text: "El producto eliminado no se puede recuperar!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Borrar',
+      cancelButtonText: 'Cancelar'
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        //Pedir a la API que borre el producto
+        try {
+          const URL = process.env.REACT_APP_API_URL + '/' + props.producto.id;
+          const respuesta = await fetch(URL, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          console.log(respuesta);
+          if (respuesta.status === 200) {
+            //Mostrar el mensaje de producto eliminado
+            Swal.fire(
+              'Producto Eliminado!',
+              'El producto fue correctamente eliminado.', 
+              'success'
+            )
+            //Consultar API para que me cargue la lista de producto
+            props.consultarAPI();
+          }
+        } catch (error) {
+          console.log(error);
+
+        }
+
+      }
+    })
+  }
   return (
     <ListGroup.Item className="d-flex justify-content-between">
       <p>
@@ -14,7 +55,7 @@ const ItemProducto = (props) => {
       </p>
       <div>
         <Link to={`/productos/editar/${props.producto.id}`} className='btn btn-warning me-2'>Editar</Link>
-        <Button variant="danger" >
+        <Button variant="danger" onClick={() => { eliminarProducto() }} >
           Borrar
         </Button>
       </div>
